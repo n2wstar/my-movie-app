f3b378cdd8999c34f61d9b0319740369d52c248f
SB J <>
2020-03-16 18:20:56 +0900

    add navigation

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,23 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ year, title, summary, poster, genres = []}) {
+function Movie({ id, year, title, summary, poster, genres = []}) {
   return (
     <div className="movie">
-      <img className="movie__name" src={poster} alt={title} title={title} />
-      <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <h5 className="movie__year">{year}</h5>
-        <ul className="genres">
-            {genres.map((genre, index) => (
-                <li key={index} className="genres__genre"> 
-                {genre} <span> . </span>
-                </li>
-            ))}
-        </ul>
-        <p className="movie__summary">{summary.slice(0,200)}...</p>
-      </div>
+      <Link 
+        to = {{
+          pathname: `movie-movie/${id}`,
+          state: {
+            year,
+            title,
+            summary,
+            poster,
+            genres
+          }
+        }}
+        >
+        <div className="movie">
+          <img className="movie__name" src={poster} alt={title} title={title} />
+          <div className="movie__data">
+            <h3 className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+            <ul className="genres">
+                {genres.map((genre, index) => (
+                    <li key={index} className="genres__genre"> 
+                    {genre} <span> . </span>
+                    </li>
+                ))}
+            </ul>
+            <p className="movie__summary">{summary.slice(0,200)}...</p>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 }
--- src/routes/About.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function About(){
-    return <span>about this page</span>
-}
-
-export default About;
