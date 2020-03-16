b6aaa67b11cf32229363fc811e63f2ea87dff5a0
SB J <>
2020-03-16 13:57:56 +0900

    Updates

---
+++ precache-manifest.d32c8d89f7e6ab830919d5e627a0f92f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32aa64e7db4338817515f3a988a5bcb3",
+    "url": "/n2wstar/my-movie-app/index.html"
+  },
+  {
+    "revision": "0ab94f9db92d704520dd",
+    "url": "/n2wstar/my-movie-app/static/css/main.f5152498.chunk.css"
+  },
+  {
+    "revision": "c655f3e493da6bf96af5",
+    "url": "/n2wstar/my-movie-app/static/js/2.bf5bd27e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/n2wstar/my-movie-app/static/js/2.bf5bd27e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ab94f9db92d704520dd",
+    "url": "/n2wstar/my-movie-app/static/js/main.5734db38.chunk.js"
+  },
+  {
+    "revision": "e7be591799a7f599436d",
+    "url": "/n2wstar/my-movie-app/static/js/runtime-main.9d41b63d.js"
+  }
+]);
